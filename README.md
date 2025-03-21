e6af818b432068840becbba8f8b68089165a25b9
srajan-kush <144719261+srajan-kush@users.noreply.github.com>
2025-03-21 16:30:31 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# mini c compiler
- 
+# Frontend Phase of a C Compiler
+
+## Using the Compiler
+
+```
+lex lexer.l
+yacc -d -v parser.y
+gcc -w y.tab.c -o parser
+./parser < input.c
+```
+
+
---
+++ input.c
@@ -0,0 +1,89 @@
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+    int a;
+    int x=1;
+    int y=2;
+    int z=3;
+    x=3;
+    y=10;
+    z=5;
+    if(x>5) {
+        for(int k=0; k<10; k++) {
+            y = x+3;
+            printf("Hello!");
+        }
+    } else {
+        int idx = 1;
+    }
+    for(int i=0; i<10; i++) {
+        printf("Hello World!");
+        scanf("%d", &x);
+        if (x>5) {
+            printf("Hi");
+        }
+        for(int j=0; j<z; j++) {
+            a=1;
+        }
+    } 
+    return 1;
+}
+
+
+
+// #include<stdio.h>
+// #include<string.h>
+
+// int main() {
+//     int x=1;
+//     float f;
+//     int a=3;
+//     int x;
+//     a = x * 3 + 5;
+//     if(x>a) {
+//         printf("Hi!");
+//         a = x * 3 + 100;
+//         if(x>a) {
+//             printf("Hi!");
+//             a = x * 3 + 100;
+//         }
+//         else {
+//             x = a * 3 + 100;
+//         }
+//     }
+//     else {
+//         x = a * 3 + 100;
+//     }
+// }
+
+
+// #include <stdio.h>
+
+// int main() {
+//     int n = 10; 
+//     int first = 0;
+//     int second = 1; 
+//     int next;
+//     int n;
+
+//     printf("Enter the number of terms: ");
+//     scanf("%d", &n);
+
+//     printf("Fibonacci Series: ");
+
+//     for(int i=0; i<10; i++){
+//         printf("HI!");
+
+//         next = first + second;
+//         first = second;
+//         second = next;
+//     }
+
+//     return 0;
+// }
+
+
+
+
+
